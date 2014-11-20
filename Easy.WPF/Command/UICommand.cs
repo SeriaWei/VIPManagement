a12fef59402ea5a8829ea7074f59693795684b3b
Wayne <seriawei@gmail.com>
2014-11-20 15:03:55 +0800

    UICommand

---
+++ Easy.WPF/Command/UICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace Easy.WPF.Command
+{
+    public abstract class UICommand : RoutedUICommand
+    {
+        public abstract void Executed(object target, ExecutedRoutedEventArgs e);
+        public abstract void CanExecute(object sender, CanExecuteRoutedEventArgs e);
+    }
+}
---
+++ Easy.WPF/Command/UICommandBinding.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace Easy.WPF.Command
+{
+    public class UICommandBinding : CommandBinding
+    {
+        public UICommandBinding(UICommand command)
+            : base(command, command.Executed, command.CanExecute)
+        {
+        }
+    }
+}
