35c4e60a0ffa38fa22a5f1d3f8f0741712387f5d
Wayne <seriawei@outlook.com>
2014-12-03 21:05:02 +0800

    F

---
+++ VIP.Core/Card/CardEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Easy.Models;
+
+namespace VIP.Core.Card
+{
+    public class CardEntity : EditorEntity
+    {
+        public string CardID { get; set; }
+        public string CardNumber { get; set; }
+        public double Rebate { get; set; }
+        public int CardCategory { get; set; }
+    }
+}
